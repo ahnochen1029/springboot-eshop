f801e2d070ca828551a5bb3b07800d414348e4f2
AhnoChen <ahnochen@wistronits.com>
2023-01-22 10:43:53 +0800

    init

---
+++ src/main/java/com/ahnochen/springbooteshop/SpringbootEshopApplication.java
@@ -0,0 +1,13 @@
+package com.ahnochen.springbooteshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahnochen/springbooteshop/SpringbootEshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahnochen.springbooteshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
